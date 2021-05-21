45d2fb98bd28566679a0f27705bec85689697d14
SIddharth3791 <spatel3791@gmail.com>
2021-05-20 21:54:04 -0400

    Introduce Concept of Spring Config Server

--- order-service/src/main/java/com/sid/orderservice/api/service/OrderService.java
+++ order-service/src/main/java/com/sid/orderservice/api/service/OrderService.java
@@ -1,6 +1,9 @@
 package com.sid.orderservice.api.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,13 +14,18 @@ import com.sid.orderservice.api.model.Order;
 import com.sid.orderservice.api.repo.OrderRepo;
 
 @Service
+@RefreshScope
 public class OrderService {
 	
 	@Autowired
 	private OrderRepo repo;
 	
 	@Autowired
+	@Lazy
 	private RestTemplate template;
+	
+	@Value("${payment-service.endpoints.endpoint}")
+	private String PAYMENT_ENDPOINT_URL;
 	
 	public TransactionResponse saveOrder(TransactionRequest request) {
 		
@@ -26,7 +34,7 @@ public class OrderService {
 		payment.setOrderId(order.getId());
 		payment.setAmount(order.getPrice());
 		
-		Payment paymentResponse = template.postForObject("http://PAYMENT-SERVICE/payment/pay", payment, Payment.class);
+		Payment paymentResponse = template.postForObject(PAYMENT_ENDPOINT_URL, payment, Payment.class);
 		
 		repo.save(order);
 		
